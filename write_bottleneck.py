c2aeab618fb508661320e5778142c8b01d900454
elninowang <elninowang@gmail.com>
2017-09-12 20:28:58 +0800

    add data-visual.ipynb

--- write_bottleneck.py
+++ write_bottleneck.py
@@ -22,9 +22,18 @@ def write_gap(MODEL, image_size, lambda_func=None):
     model = Model(base_model.input, GlobalAveragePooling2D()(base_model.output))
 
     print(MODEL.__name__)
+    train_gen = ImageDataGenerator(
+        featurewise_std_normalization=False,
+        samplewise_std_normalization=False,
+        rotation_range=10.,
+        width_shift_range=0.05,
+        height_shift_range=0.05,
+        shear_range=0.1,
+        zoom_range=0.1,
+    )
     gen = ImageDataGenerator()
     batch_size = 16
-    train_generator = gen.flow_from_directory(os.path.join(dir, 'train'), image_size, shuffle=False, batch_size=batch_size)
+    train_generator = train_gen.flow_from_directory(os.path.join(dir, 'train'), image_size, shuffle=False, batch_size=batch_size, save_to_dir="save_images/train")
     print("subdior to train type {}".format(train_generator.class_indices))
     valid_generator = gen.flow_from_directory(os.path.join(dir, 'valid'), image_size, shuffle=False, batch_size=batch_size)
     print("subdior to valid type {}".format(valid_generator.class_indices))
@@ -39,7 +48,7 @@ def write_gap(MODEL, image_size, lambda_func=None):
     print("valid label: {}".format(valid_generator.classes.shape))
 
     print("begin create database {}".format(Model.__name__))
-    with h5py.File("models/bottleneck_%s.h5" % MODEL.__name__) as h:
+    with h5py.File("models/new/bottleneck_%s.h5" % MODEL.__name__) as h:
         h.create_dataset("train", data=train)
         h.create_dataset("valid", data=valid)
         h.create_dataset("label", data=train_generator.classes)
@@ -65,18 +74,26 @@ def write_gap_test(MODEL, image_size, lambda_func=None):
     print("test: {}".format(test.shape))
 
     print("begin create database {}".format(Model.__name__))
-    with h5py.File("models/bottleneck_%s_test.h5" % MODEL.__name__) as h:
+    with h5py.File("models/new/bottleneck_%s_test.h5" % MODEL.__name__) as h:
         h.create_dataset("test", data=test)
     print("write_gap {} successed".format(Model.__name__))
 
-# write_gap(ResNet50, (224, 224))
-# write_gap(Xception, (299, 299), xception.preprocess_input)
-# write_gap(InceptionV3, (299, 299), inception_v3.preprocess_input)
-# write_gap(VGG16, (224, 224))
-# write_gap(VGG19, (224, 224))
+def normal_preprocess_input(x):
+    x /= 255.
+    x -= 0.5
+    x *= 2
+    return x
 
-write_gap_test(ResNet50, (224, 224))
+print("===== Train & Valid =====")
+write_gap(ResNet50, (224, 224), normal_preprocess_input)
+write_gap(Xception, (299, 299), xception.preprocess_input)
+write_gap(InceptionV3, (299, 299), inception_v3.preprocess_input)
+write_gap(VGG16, (224, 224), normal_preprocess_input)
+write_gap(VGG19, (224, 224), normal_preprocess_input)
+
+print("===== Test =====")
+write_gap_test(ResNet50, (224, 224), normal_preprocess_input)
 write_gap_test(Xception, (299, 299), xception.preprocess_input)
 write_gap_test(InceptionV3, (299, 299), inception_v3.preprocess_input)
-write_gap_test(VGG16, (224, 224))
-write_gap_test(VGG19, (224, 224))
+write_gap_test(VGG16, (224, 224), normal_preprocess_input)
+write_gap_test(VGG19, (224, 224), normal_preprocess_input)
