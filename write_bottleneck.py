aff2ea1462c781417519f6b594fafbdb3a752380
elninowang <elninowang@gmail.com>
2017-09-30 22:53:48 +0800

    try RMSprop method

--- write_bottleneck.py
+++ write_bottleneck.py
@@ -11,10 +11,8 @@ import math
 
 dir = "/ext/Data/distracted_driver_detection/"
 
-def write_gap(MODEL, image_size, lambda_func=None):
-    width = image_size[0]
-    height = image_size[1]
-    input_tensor = Input((height, width, 3))
+def write_gap(tag, MODEL, image_size, lambda_func=None, featurewise_std_normalization=True):
+    input_tensor = Input((*image_size, 3))
     x = input_tensor
     if lambda_func:
         x = Lambda(lambda_func)(x)
@@ -23,17 +21,21 @@ def write_gap(MODEL, image_size, lambda_func=None):
 
     print(MODEL.__name__)
     train_gen = ImageDataGenerator(
-        featurewise_std_normalization=True,
-        samplewise_std_normalization=True,
+        featurewise_std_normalization=featurewise_std_normalization,
+        samplewise_std_normalization=False,
         rotation_range=10.,
         width_shift_range=0.05,
         height_shift_range=0.05,
         shear_range=0.1,
         zoom_range=0.1,
     )
-    gen = ImageDataGenerator()
+    gen = ImageDataGenerator(
+        featurewise_std_normalization=featurewise_std_normalization,
+        samplewise_std_normalization=False,
+    )
+
     batch_size = 16
-    train_generator = train_gen.flow_from_directory(os.path.join(dir, 'train'), image_size, shuffle=False, batch_size=batch_size, save_to_dir="save_images/train")
+    train_generator = train_gen.flow_from_directory(os.path.join(dir, 'train'), image_size, shuffle=False, batch_size=batch_size)
     print("subdior to train type {}".format(train_generator.class_indices))
     valid_generator = gen.flow_from_directory(os.path.join(dir, 'valid'), image_size, shuffle=False, batch_size=batch_size)
     print("subdior to valid type {}".format(valid_generator.class_indices))
@@ -48,17 +50,15 @@ def write_gap(MODEL, image_size, lambda_func=None):
     print("valid label: {}".format(valid_generator.classes.shape))
 
     print("begin create database {}".format(Model.__name__))
-    with h5py.File("models/new/bottleneck_%s.h5" % MODEL.__name__) as h:
+    with h5py.File(os.path.join("models", tag, "bottleneck_%s.h5") % MODEL.__name__) as h:
         h.create_dataset("train", data=train)
         h.create_dataset("valid", data=valid)
         h.create_dataset("label", data=train_generator.classes)
         h.create_dataset("valid_label", data=valid_generator.classes)
     print("write_gap {} successed".format(Model.__name__))
 
-def write_gap_test(MODEL, image_size, lambda_func=None):
-    width = image_size[0]
-    height = image_size[1]
-    input_tensor = Input((height, width, 3))
+def write_gap_test(tag, MODEL, image_size, lambda_func=None, featurewise_std_normalization=True):
+    input_tensor = Input((*image_size, 3))
     x = input_tensor
     if lambda_func:
         x = Lambda(lambda_func)(x)
@@ -66,7 +66,10 @@ def write_gap_test(MODEL, image_size, lambda_func=None):
     model = Model(base_model.input, GlobalAveragePooling2D()(base_model.output))
 
     print(MODEL.__name__)
-    gen = ImageDataGenerator()
+    gen = ImageDataGenerator(
+        featurewise_std_normalization=featurewise_std_normalization,
+        samplewise_std_normalization=False,
+    )
     batch_size = 16
     test_generator = gen.flow_from_directory(os.path.join(dir, 'test'), image_size, shuffle=False, batch_size=batch_size, class_mode=None)
     print("predict_generator test {}".format(math.ceil(test_generator.samples/batch_size)))
@@ -74,7 +77,7 @@ def write_gap_test(MODEL, image_size, lambda_func=None):
     print("test: {}".format(test.shape))
 
     print("begin create database {}".format(Model.__name__))
-    with h5py.File("models/new/bottleneck_%s_test.h5" % MODEL.__name__) as h:
+    with h5py.File(os.path.join("models", tag, "bottleneck_%s_test.h5") % MODEL.__name__) as h:
         h.create_dataset("test", data=test)
     print("write_gap {} successed".format(Model.__name__))
 
@@ -84,16 +87,56 @@ def normal_preprocess_input(x):
     x *= 2
     return x
 
-print("===== Train & Valid =====")
-write_gap(ResNet50, (224, 224), normal_preprocess_input)
-write_gap(Xception, (299, 299), xception.preprocess_input)
-write_gap(InceptionV3, (299, 299), inception_v3.preprocess_input)
-write_gap(VGG16, (224, 224), normal_preprocess_input)
-write_gap(VGG19, (224, 224), normal_preprocess_input)
+###
+### subdir = new1
+###
+if 0:
+    print("===== Train & Valid =====")
+    write_gap("new1", ResNet50, (224, 224), featurewise_std_normalization=False)
+    write_gap("new1", Xception, (299, 299), xception.preprocess_input, featurewise_std_normalization=False)
+    write_gap("new1", InceptionV3, (299, 299), inception_v3.preprocess_input, featurewise_std_normalization=False)
+    write_gap("new1", VGG16, (224, 224), featurewise_std_normalization=False)
+    write_gap("new1", VGG19, (224, 224), featurewise_std_normalization=False)
 
-print("===== Test =====")
-write_gap_test(ResNet50, (224, 224), normal_preprocess_input)
-write_gap_test(Xception, (299, 299), xception.preprocess_input)
-write_gap_test(InceptionV3, (299, 299), inception_v3.preprocess_input)
-write_gap_test(VGG16, (224, 224), normal_preprocess_input)
-write_gap_test(VGG19, (224, 224), normal_preprocess_input)
+    print("===== Test =====")
+    write_gap_test("new1", ResNet50, (224, 224), featurewise_std_normalization=False)
+    write_gap_test("new1", Xception, (299, 299), xception.preprocess_input, featurewise_std_normalization=False)
+    write_gap_test("new1", InceptionV3, (299, 299), inception_v3.preprocess_input, featurewise_std_normalization=False)
+    write_gap_test("new1", VGG16, (224, 224), featurewise_std_normalization=False)
+    write_gap_test("new1", VGG19, (224, 224), featurewise_std_normalization=False)
+
+###
+### subdir = new
+###
+if 0:
+    print("===== Train & Valid =====")
+    write_gap("new", ResNet50, (224, 224))
+    write_gap("new", Xception, (299, 299), xception.preprocess_input)
+    write_gap("new", InceptionV3, (299, 299), inception_v3.preprocess_input)
+    write_gap("new", VGG16, (224, 224))
+    write_gap("new", VGG19, (224, 224))
+
+    print("===== Test =====")
+    write_gap_test("new", ResNet50, (224, 224))
+    write_gap_test("new", Xception, (299, 299), xception.preprocess_input)
+    write_gap_test("new", InceptionV3, (299, 299), inception_v3.preprocess_input)
+    write_gap_test("new", VGG16, (224, 224))
+    write_gap_test("new", VGG19, (224, 224))
+
+###
+### subdir = noscale
+###
+if 1:
+    print("===== Train & Valid =====")
+    write_gap("noscale", ResNet50, (240, 320))
+    write_gap("noscale", Xception, (360, 480), xception.preprocess_input)
+    write_gap("noscale", InceptionV3, (360, 480), inception_v3.preprocess_input)
+    write_gap("noscale", VGG16, (240, 320))
+    write_gap("noscale", VGG19, (240, 320))
+
+    print("===== Test =====")
+    write_gap_test("noscale", ResNet50, (240, 320))
+    write_gap_test("noscale", Xception, (360, 480), xception.preprocess_input)
+    write_gap_test("noscale", InceptionV3, (360, 480), inception_v3.preprocess_input)
+    write_gap_test("noscale", VGG16, (240, 320))
+    write_gap_test("noscale", VGG19, (240, 320))
